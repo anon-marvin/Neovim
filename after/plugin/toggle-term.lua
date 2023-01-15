57c58d541a950e3913e57b3e6ac8214d5cce7f34
anon-marvin <anon.marvin.sohan@gmail.com>
2023-01-15 21:57:19 +0530

    Terminal Support Added

--- after/plugin/toggle-term.lua
+++ after/plugin/toggle-term.lua
@@ -3,14 +3,14 @@ require"toggleterm".setup {
   open_mapping = [[<c-\>]],
   shade_filetypes = {},
   shade_terminals = true,
-  shading_factor = '50',
+  shading_factor = '2',
   start_in_insert = true,
   persist_size = true,
   direction = 'float',
   float_opts = {
     border = 'curved',
     width = 100,
-    height = 25,
+    height = 20,
     winblend = 3,
   },
   winbar = {
