86123a93dc8985660ce36f0480770dcf8dfd4ecc
anon-marvin <anon.marvin.sohan@gmail.com>
2023-01-26 09:31:18 +0530

    Floating terminal and remaps added

--- after/plugin/toggle-term.lua
+++ after/plugin/toggle-term.lua
@@ -3,16 +3,10 @@ require"toggleterm".setup {
   open_mapping = [[<c-\>]],
   shade_filetypes = {},
   shade_terminals = true,
-  shading_factor = '2',
+  shading_factor = '25',
   start_in_insert = true,
   persist_size = true,
-  direction = 'float',
-  float_opts = {
-    border = 'curved',
-    width = 100,
-    height = 20,
-    winblend = 3,
-  },
+  direction = 'horizontal',
   winbar = {
     enabled = false,
     name_formatter = function(term) --  term: Terminal
--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,13 +1,19 @@
 local wk = require("which-key")
+local Terminal = require('toggleterm.terminal').Terminal
+local toggle_float = function ()
+    local float = Terminal:new({direction = 'float'})
+    return float:toggle()
+end
 local mappings={
     q= {":q!<cr>","Quit"},
     w= {":w<cr>","Save"},
     x= {":bdelete<cr>","Close"},
     Q= {":wq!<cr>","Save & Quit"},
-    f= {":Telescope find_files<cr>" ,"Search"},
+    s= {":Telescope find_files<cr>" ,"Search"},
     g= {":Telescope live_grep<cr>"  ,"Grep"},
     r= {":RunCode<cr>" , "RunCode"},
-    l= {":terminal open %<cr>" ,"Live-Server"}
+    t={":ToggleTerm<cr>","Split Below"},
+    f ={toggle_float,"Floating Terminal"}
   }
-   local opts={prefix='<leader>'}
+local opts={prefix='<leader>'}
 wk.register(mappings, opts)
